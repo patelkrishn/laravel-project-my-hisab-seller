6db500b16617988e84c1913ced64a42978510a4f
lninfosystem <48345319+lninfosystem@users.noreply.github.com>
2020-03-06 11:06:08 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    E-hisab
+                    E-Hisab
                 </div>
 
                 <div class="links">
