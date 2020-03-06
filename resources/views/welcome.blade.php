440d4fa52fe34cc8e3dee0c8a12078b485885965
lninfosystem <48345319+lninfosystem@users.noreply.github.com>
2020-03-06 11:01:15 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    E-hisab
                 </div>
 
                 <div class="links">
